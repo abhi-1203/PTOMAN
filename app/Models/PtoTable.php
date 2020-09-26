672af8f90a2d33e7ae2ee8383165f839aebd87d5
Abhijith <abhijith1203@gmail.com>
2020-09-26 20:39:21 +0530

    PTOMAN

---
+++ app/Models/PtoTable.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PtoTable extends Model
+{
+    use HasFactory;
+
+    protected $table = 'apacrp_pto';
+
+
+}
